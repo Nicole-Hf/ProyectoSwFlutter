56c4035f1795e034391d5026e463e7cb97e25337
LizReina <lizfernanda.rq@gmail.com>
2022-06-22 05:57:58 +0200

    prueba

--- lib/pages/microbus_page.dart
+++ lib/pages/microbus_page.dart
@@ -8,6 +8,32 @@ class MicrobusPage extends StatefulWidget {
 }
 
 class _MicrobusPageState extends State<MicrobusPage> {
+  String _placa= '';
+  String _modelo= '';
+  String _nro_asientos= '';
+  String _nro_linea= '';
+  String _nroInterno= '';
+  String _fecha_asignacion= '';
+  String _fecha_baja= '';
+
+  createAccountPressed() async {
+    http.Response response = await AuthServices.conductorRegister(_name, _fechanacimiento,
+      _ci,_telefono,_categorialic,);
+    Map responseMap = jsonDecode(response.body);
+    var dataUser = json.decode(response.body);
+    if (response.statusCode == 401) {
+      print("datos registrados");
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (BuildContext context) => const MicrobusPage(),
+          ));
+    } else {
+      // ignore: use_build_context_synchronously
+      errorSnackBar(context, responseMap.values.first[0]);
+    }
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,15 +117,70 @@ class _MicrobusPageState extends State<MicrobusPage> {
     ],
     ),
     ),
+      Column(
+        children: [
+          const SizedBox(height: 20,),
+          TextField(
+            decoration: const InputDecoration(hintText: 'placa'),
+            onChanged: (value) {
+              _placa = value;
+            },
+          ),
+          const SizedBox(height: 20,),
+          TextField(
+            decoration: const InputDecoration(hintText: 'modelo'),
+            onChanged: (value) {
+              _modelo = value;
+            },
+          ),
+          const SizedBox(height: 20,),
+          TextField(
+            decoration: const InputDecoration(hintText: 'numero de asientos'),
+            onChanged: (value) {
+              _nro_asientos = value;
+            },
+          ),
 
-      buildTextField("placa", "ejm: GX34J"),
+          const SizedBox(height: 20,),
+          TextField(
+            decoration: const InputDecoration(hintText: 'numero de linea'),
+            onChanged: (value) {
+              _nro_linea = value;
+            },
+          ),
+
+          const SizedBox(height: 20,),
+          TextField(
+            decoration: const InputDecoration(hintText: 'numero interno'),
+            onChanged: (value) {
+              _nroInterno = value;
+            },
+          ),
+
+          const SizedBox(height: 20,),
+          TextField(
+            decoration: const InputDecoration(hintText: 'fecha de asignacion'),
+            onChanged: (value) {
+              _fecha_asignacion = value;
+            },
+          ),
+
+          const SizedBox(height: 20,),
+          TextField(
+            decoration: const InputDecoration(hintText: 'fecha de baja'),
+            onChanged: (value) {
+              _fecha_baja = value;
+            },
+          ),
+          const SizedBox(height: 20,),
+      /*buildTextField("placa", "ejm: GX34J"),
       buildTextField("modelo", "ejm: suzuki"),
       buildTextField("numero de asientos", "ejm: 10"),
       buildTextField("numero de linea", "ejm: 52"),
       buildTextField("numero inteno", "ejm: A4567843"),
       buildTextField("fecha de asignacion", "ejm: año/mes/dia"),
       buildTextField("fecha de baja", "ejm: año/mes/dia"),
-      const SizedBox(height: 0),
+      const SizedBox(height: 0),*/
 
       Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -66,4 +66,30 @@ class AuthServices {
     print(response.body);
     return response;
   }
+
+  static Future<http.Response> microbusRegister(String placa, String modelo, int nro_asientos, String telefono,
+      String categorialic) async {
+    Map data = {
+      "placa": name,
+      "modelo": fechanacimiento,
+      "nro_asientos": ci,
+      "nro_linea": telefono,
+      "nroInterno": categorialic,
+      "fecha_asignacion": ci,
+      "fecha_baja": telefono,
+      "nroInterno": categorialic,
+      "conductor_id": idConductor,
+    };
+
+    var body = json.encode(data);
+    var url = Uri.parse('${baseUrl}auth/UserConductor');
+    http.Response response = await http.post(
+        url,
+        headers: headers,
+        body: body
+    );
+    // ignore: avoid_print
+    print(response.body);
+    return response;
+  }
 }
