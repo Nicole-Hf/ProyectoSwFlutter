aff3d4ea18f6f5158c17a684139669b84d67af2f
Nicole Herbas <78878905+Nicole-Hf@users.noreply.github.com>
2022-07-28 10:23:53 -0400

    cambios

--- lib/services/recorrido_service.dart
+++ lib/services/recorrido_service.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:rutas_microbuses/constant.dart';
 import 'package:http/http.dart' as http;
-import 'package:rutas_microbuses/services/user_service.dart';
 import 'package:rutas_microbuses/variables.dart';
 
   Future<http.Response> createRecorrido(String? tipo) async {
@@ -31,13 +30,11 @@ import 'package:rutas_microbuses/variables.dart';
     };
 
     var body = json.encode(data);
-    String token = await getToken();
     var url = Uri.parse('$updateLocationUrl/$idRecorrido');
     http.Response response = await http.put(
       url,
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token'
       },
       body: body
     );
@@ -59,21 +56,19 @@ import 'package:rutas_microbuses/variables.dart';
 
   Future<http.Response> saveRetiro(String? motivo) async {
     Map data = {
-      "motivo": latitud,
+      "motivo": motivo,
       "recorrido_id": idRecorrido,
     };
 
     var body = json.encode(data);
-    String token = await getToken();
     var url = Uri.parse(saveRetiroUrl);
     http.Response response = await http.post(
       url,
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer $token'
       },
       body: body
     );
-    debugPrint('Body Tracking: ${response.body}');
+    debugPrint('Body Retiro: ${response.body}');
     return response;
   }
